79b0e06d161b36f243cddb3730f406b95510e3e3
luis sanchez <luis.felip.sanch@gmail.com>
2025-01-06 19:42:38 -0700

    fixed missing import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import playwright from 'eslint-plugin-playwright'
+import { FlatCompat } from '@eslint/eslintrc'
 
 const compat = new FlatCompat({
     // import.meta.dirname is available after Node.js v20.11.0
