3d0582e81f21df6109c42d6b52b067403f3fb2d7
luis sanchez <luis.felip.sanch@gmail.com>
2025-01-09 11:07:47 -0700

    debugging auth issue in prod

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
-import { config } from 'dotenv';
-import { defineConfig } from "drizzle-kit";
+import { config } from 'dotenv'
+import { defineConfig } from 'drizzle-kit'
 
-config({ path: '.env' });
+config()
 
 export default defineConfig({
-    schema: "./src/db/schema.ts",
-    out: "./migrations",
-    dialect: "postgresql",
+    schema: './src/db/schema.ts',
+    out: './migrations',
+    dialect: 'postgresql',
     dbCredentials: {
         url: process.env.POSTGRES_URL!,
     },
-});
+})
--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,13 @@
-import NextAuth from "next-auth"
-import { authConfig } from "./auth.config"
-import { DrizzleAdapter } from "@auth/drizzle-adapter"
-import { db } from "./db/drizzle"
+import NextAuth from 'next-auth'
+import { authConfig } from './auth.config'
+import { DrizzleAdapter } from '@auth/drizzle-adapter'
+import { db } from './db/drizzle'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: DrizzleAdapter(db),
+    debug: true,
     session: {
-        strategy: "jwt",
+        strategy: 'jwt',
     },
     ...authConfig,
 })
