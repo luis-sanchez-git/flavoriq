c6a21005b4dd23870c6ac13edffae7c63737b01b
luis sanchez <luis.felip.sanch@gmail.com>
2025-01-22 20:44:26 -0700

    fixed go back url

--- src/app/meal-baskets/layout.tsx
+++ src/app/meal-baskets/layout.tsx
@@ -1,16 +1,25 @@
+'use client'
+
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { ArrowLeft } from 'lucide-react'
+import { usePathname } from 'next/navigation'
 
 export default function MealBasketsLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
+    const path = usePathname()
+    const paths = path.split('/')
+    let href = '/recipes'
+    if (paths.length > 2) {
+        href = paths.slice(0, -1).join('/')
+    }
     return (
         <section className="flex-1">
             <div className="container mx-auto px-4 py-8 h-full">
-                <Link href="/recipes">
+                <Link href={href}>
                     <Button>
                         <ArrowLeft className="mr-2 h-4 w-4" />
                         Go Back
